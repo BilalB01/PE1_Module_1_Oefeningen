f0084ebd2547a8a02f3e1d067da68339ad225a1d
Bouchta Bilal <161035575+BilalB01@users.noreply.github.com>
2024-06-03 21:54:22 +0200

    Update e1_beginnen.mjs

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -1,5 +1,8 @@
 // Voeg hier je eigen code in
 
 
+let name = "Bilal";
+let age = '24';
+let favoriteGame = ' blackops2';
 
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
